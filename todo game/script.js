e3cea39173d81dfdece495bb35c1ccc0c4b5650e
MFarooq78612 <fk3942446@gmail.com>
2024-05-28 22:08:31 +0500

    Web apps
    Web apps using html css and js

---
+++ todo game/script.js
@@ -0,0 +1,50 @@
+// let div=document.querySelector("div");
+// let ul=document.querySelector("ul");
+// let lis=document.querySelectorAll("li");
+
+// div.addEventListener("click",function(){
+// console.log("div was clicked.")
+// })
+
+// ul.addEventListener("click",function(event){
+//     event.stopPropagation();
+// console.log("ul was clicked.")
+
+// })
+
+
+
+
+// for(li of lis){
+//     li.addEventListener("click",function(event){
+//         event.stopPropagation();
+//         console.log("list was clicked.")
+//         })
+// }
+
+
+// let btn=document.querySelector("button");
+// let input=document.querySelector("input");
+// let ul=document.querySelector("ul");
+
+// btn.addEventListener('click',function(){
+//     let item=document.createElement("li");
+//     item.innerText=input.value;
+//     ul.appendChild(item);
+//     let delBtn=document.createElement("button");
+//     delBtn.innerText="delete";
+//     delBtn.classList.add("delete");
+//     item.appendChild(delBtn);
+//     input.value=""; 
+// })
+
+
+// ul.addEventListener('click',function(event){
+// if(event.target.nodeName=="BUTTON"){
+//     let listItem=event.target.parentElement;
+//     listItem.remove();
+//     console.log("deleted");
+
+// }
+// })
+
